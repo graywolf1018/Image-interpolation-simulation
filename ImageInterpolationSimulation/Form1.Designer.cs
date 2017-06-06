8203ca3703cd210bc53d276ea0c26df5f8ae3ebe
Young <gray_wolf3000@yahoo.com.tw>
2017-06-06 16:15:16 +0800

    1.0.0
    Just run some interpolation methods

---
+++ ImageInterpolationSimulation/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace ImageInterpolationSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnLoadOrigianl = new System.Windows.Forms.Button();
+            this.btnCFA = new System.Windows.Forms.Button();
+            this.btnBilinear = new System.Windows.Forms.Button();
+            this.picCFA = new System.Windows.Forms.PictureBox();
+            this.picBilinear = new System.Windows.Forms.PictureBox();
+            this.lblPSNR_BI = new System.Windows.Forms.Label();
+            this.lblPSNR_CFA = new System.Windows.Forms.Label();
+            this.picEPBI = new System.Windows.Forms.PictureBox();
+            this.btnEPBI = new System.Windows.Forms.Button();
+            this.lblPSNR_EPBI = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCFA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBilinear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEPBI)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picOriginal.Location = new System.Drawing.Point(12, 12);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(200, 200);
+            this.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picOriginal.TabIndex = 0;
+            this.picOriginal.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnLoadOrigianl
+            // 
+            this.btnLoadOrigianl.Location = new System.Drawing.Point(12, 218);
+            this.btnLoadOrigianl.Name = "btnLoadOrigianl";
+            this.btnLoadOrigianl.Size = new System.Drawing.Size(200, 23);
+            this.btnLoadOrigianl.TabIndex = 1;
+            this.btnLoadOrigianl.Text = "Load Image";
+            this.btnLoadOrigianl.UseVisualStyleBackColor = true;
+            this.btnLoadOrigianl.Click += new System.EventHandler(this.btnLoadOrigianl_Click);
+            // 
+            // btnCFA
+            // 
+            this.btnCFA.Location = new System.Drawing.Point(218, 218);
+            this.btnCFA.Name = "btnCFA";
+            this.btnCFA.Size = new System.Drawing.Size(200, 23);
+            this.btnCFA.TabIndex = 2;
+            this.btnCFA.Text = "CFA Filter";
+            this.btnCFA.UseVisualStyleBackColor = true;
+            this.btnCFA.Click += new System.EventHandler(this.btnCFA_Click);
+            // 
+            // btnBilinear
+            // 
+            this.btnBilinear.Location = new System.Drawing.Point(424, 218);
+            this.btnBilinear.Name = "btnBilinear";
+            this.btnBilinear.Size = new System.Drawing.Size(200, 23);
+            this.btnBilinear.TabIndex = 3;
+            this.btnBilinear.Text = "Bilinear Interpolation";
+            this.btnBilinear.UseVisualStyleBackColor = true;
+            this.btnBilinear.Click += new System.EventHandler(this.btnBilinear_Click);
+            // 
+            // picCFA
+            // 
+            this.picCFA.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCFA.Location = new System.Drawing.Point(218, 12);
+            this.picCFA.Name = "picCFA";
+            this.picCFA.Size = new System.Drawing.Size(200, 200);
+            this.picCFA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCFA.TabIndex = 4;
+            this.picCFA.TabStop = false;
+            // 
+            // picBilinear
+            // 
+            this.picBilinear.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBilinear.Location = new System.Drawing.Point(424, 12);
+            this.picBilinear.Name = "picBilinear";
+            this.picBilinear.Size = new System.Drawing.Size(200, 200);
+            this.picBilinear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBilinear.TabIndex = 5;
+            this.picBilinear.TabStop = false;
+            // 
+            // lblPSNR_BI
+            // 
+            this.lblPSNR_BI.Location = new System.Drawing.Point(422, 244);
+            this.lblPSNR_BI.Name = "lblPSNR_BI";
+            this.lblPSNR_BI.Size = new System.Drawing.Size(202, 23);
+            this.lblPSNR_BI.TabIndex = 6;
+            this.lblPSNR_BI.Text = "Bilinear PSNR";
+            this.lblPSNR_BI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPSNR_CFA
+            // 
+            this.lblPSNR_CFA.Location = new System.Drawing.Point(216, 244);
+            this.lblPSNR_CFA.Name = "lblPSNR_CFA";
+            this.lblPSNR_CFA.Size = new System.Drawing.Size(202, 23);
+            this.lblPSNR_CFA.TabIndex = 6;
+            this.lblPSNR_CFA.Text = "CFA PSNR";
+            this.lblPSNR_CFA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // picEPBI
+            // 
+            this.picEPBI.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picEPBI.Location = new System.Drawing.Point(630, 12);
+            this.picEPBI.Name = "picEPBI";
+            this.picEPBI.Size = new System.Drawing.Size(200, 200);
+            this.picEPBI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picEPBI.TabIndex = 5;
+            this.picEPBI.TabStop = false;
+            // 
+            // btnEPBI
+            // 
+            this.btnEPBI.Location = new System.Drawing.Point(631, 218);
+            this.btnEPBI.Name = "btnEPBI";
+            this.btnEPBI.Size = new System.Drawing.Size(200, 23);
+            this.btnEPBI.TabIndex = 3;
+            this.btnEPBI.Text = "EP-Bilinear Interpolation";
+            this.btnEPBI.UseVisualStyleBackColor = true;
+            this.btnEPBI.Click += new System.EventHandler(this.btnEPBI_Click);
+            // 
+            // lblPSNR_EPBI
+            // 
+            this.lblPSNR_EPBI.Location = new System.Drawing.Point(628, 244);
+            this.lblPSNR_EPBI.Name = "lblPSNR_EPBI";
+            this.lblPSNR_EPBI.Size = new System.Drawing.Size(202, 23);
+            this.lblPSNR_EPBI.TabIndex = 6;
+            this.lblPSNR_EPBI.Text = "EP-Bilinear PSNR";
+            this.lblPSNR_EPBI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 273);
+            this.Controls.Add(this.lblPSNR_CFA);
+            this.Controls.Add(this.lblPSNR_EPBI);
+            this.Controls.Add(this.lblPSNR_BI);
+            this.Controls.Add(this.picEPBI);
+            this.Controls.Add(this.picBilinear);
+            this.Controls.Add(this.picCFA);
+            this.Controls.Add(this.btnEPBI);
+            this.Controls.Add(this.btnBilinear);
+            this.Controls.Add(this.btnCFA);
+            this.Controls.Add(this.btnLoadOrigianl);
+            this.Controls.Add(this.picOriginal);
+            this.Name = "Form1";
+            this.Text = "Image Interpolation Simulation @ NCTU Communaction Lab";
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCFA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBilinear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEPBI)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnLoadOrigianl;
+        private System.Windows.Forms.Button btnCFA;
+        private System.Windows.Forms.Button btnBilinear;
+        private System.Windows.Forms.PictureBox picCFA;
+        private System.Windows.Forms.PictureBox picBilinear;
+        private System.Windows.Forms.Label lblPSNR_BI;
+        private System.Windows.Forms.Label lblPSNR_CFA;
+        private System.Windows.Forms.PictureBox picEPBI;
+        private System.Windows.Forms.Button btnEPBI;
+        private System.Windows.Forms.Label lblPSNR_EPBI;
+    }
+}
+
---
+++ ImageInterpolationSimulation/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageInterpolationSimulation
+{
+    public partial class Form1 : Form
+    {
+        private int[, ,] GetImgData(Bitmap myBitmap)
+        {
+            int[, ,] ImgData = new int[myBitmap.Width, myBitmap.Height, 3];
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, myBitmap.Width, myBitmap.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int ByteOfSkip = byteArray.Stride - byteArray.Width * 3;
+            unsafe
+            {
+                byte* imgPtr = (byte*)(byteArray.Scan0);
+                for (int y = 0; y < byteArray.Height; y++)
+                {
+                    for (int x = 0; x < byteArray.Width; x++)
+                    {
+                        ImgData[x, y, 2] = (int)*(imgPtr);
+                        ImgData[x, y, 1] = (int)*(imgPtr + 1);
+                        ImgData[x, y, 0] = (int)*(imgPtr + 2);
+                        imgPtr += 3;
+                    }
+                    imgPtr += ByteOfSkip;
+                }
+            }
+
+            myBitmap.UnlockBits(byteArray);
+            return ImgData;
+        }
+        public static Bitmap CreateBitmap(int[, ,] ImgData)
+        {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+            Bitmap myBitmap = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+
+            BitmapData byteArray = myBitmap.LockBits(new Rectangle(0, 0, Width, Height),
+                                           ImageLockMode.WriteOnly,
+                                           PixelFormat.Format24bppRgb);
+            //Padding bytes的長度
+            int ByteOfSkip = byteArray.Stride - myBitmap.Width * 3;
+            unsafe
+            {
+                byte* imgPtr = (byte*)byteArray.Scan0;
+                for (int y = 0; y < Height; y++)
+                {
+                    for (int x = 0; x < Width; x++)
+                    {
+                        *imgPtr = (byte)ImgData[x, y, 2];       //B
+
+                        *(imgPtr + 1) = (byte)ImgData[x, y, 1];   //G 
+
+                        *(imgPtr + 2) = (byte)ImgData[x, y, 0];   //R  
+                        imgPtr += 3;
+                    }
+                    imgPtr += ByteOfSkip; // 跳過Padding bytes
+                }
+            }
+            myBitmap.UnlockBits(byteArray);
+            return myBitmap;
+        }
+        Bitmap originalBitmap = null;
+        Bitmap CFABitmap = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private double PSNR(int[, ,] ImgData1, int[, ,] ImgData2, double IgnorePix = 15) {
+            double Width = ImgData1.GetLength(0);
+            double Height = ImgData2.GetLength(1);
+            double MSE = 0;
+            for (int y = (int)IgnorePix ; y < Height - (int)IgnorePix ; y++) {
+                for (int x = (int)IgnorePix ; x < Width - (int)IgnorePix ; x++) {
+                    double e_R = Math.Pow(Convert.ToDouble(ImgData1[x, y, 0]) - Convert.ToDouble(ImgData2[x, y, 0]), 2);
+                    double e_G = Math.Pow(Convert.ToDouble(ImgData1[x, y, 1]) - Convert.ToDouble(ImgData2[x, y, 1]), 2);
+                    double e_B = Math.Pow(Convert.ToDouble(ImgData1[x, y, 2]) - Convert.ToDouble(ImgData2[x, y, 2]), 2);
+                    double pixMSE = (e_R + e_G + e_B) / 3.0;
+                    MSE += pixMSE;
+                }
+            }
+            MSE = MSE / (Width - IgnorePix * 2) / (Height - IgnorePix * 2);
+            return 10.0 * Math.Log10(255.0 * 255.0 / MSE);
+            //return MSE;
+        }
+        private int Mid(int p1, int p2, int p3, int p4) {
+            double w = Convert.ToDouble(p1);
+            double x = Convert.ToDouble(p2);
+            double y = Convert.ToDouble(p3);
+            double z = Convert.ToDouble(p4);
+            double total = w + x + y + z;
+            double minVal = Math.Min(w, Math.Min(x, Math.Min(y, z)));
+            double maxVal = Math.Max(w, Math.Max(x, Math.Max(y, z)));
+            return (int)((total - maxVal - minVal) / 2.0);
+        }
+        private void btnLoadOrigianl_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)   ////由對話框選取圖檔
+            {
+                originalBitmap = new Bitmap(openFileDialog1.FileName);
+                picOriginal.Image = originalBitmap;
+            }
+        }
+        private void btnCFA_Click(object sender, EventArgs e)
+        {
+            int[, ,] ImgData = GetImgData(originalBitmap);
+            CFAProcess(ImgData);
+            CFABitmap = CreateBitmap(ImgData);
+            picCFA.Image = CFABitmap;
+
+            int[, ,] OriginalData = GetImgData(originalBitmap);
+            lblPSNR_CFA.Text = Convert.ToString(PSNR(OriginalData, ImgData));
+        }
+        private void CFAProcess(int[, ,] ImgData)
+        {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if ((x % 2 == 0) && (y % 2 == 0))
+                    {
+                        ImgData[x, y, 0] = 0;
+                        ImgData[x, y, 2] = 0;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 0))
+                    {
+                        ImgData[x, y, 1] = 0;
+                        ImgData[x, y, 2] = 0;
+                    }
+                    else if ((x % 2 == 0) && (y % 2 == 1))
+                    {
+                        ImgData[x, y, 0] = 0;
+                        ImgData[x, y, 1] = 0;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 1))
+                    {
+                        ImgData[x, y, 0] = 0;
+                        ImgData[x, y, 2] = 0;
+                    }
+                }
+            }
+        }
+        private void btnBilinear_Click(object sender, EventArgs e)
+        {
+            int[, ,] ImgData = GetImgData(CFABitmap);
+            BIProcess(ImgData);
+            Bitmap processedBitmap = CreateBitmap(ImgData);
+            picBilinear.Image = processedBitmap;
+
+            int[, ,] OriginalData = GetImgData(originalBitmap);
+            lblPSNR_BI.Text = Convert.ToString(PSNR(OriginalData, ImgData));
+        }
+        private void BIProcess(int[, ,] ImgData)
+        {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+
+            for (int y = 1; y < Height - 1; y++)
+            {
+                for (int x = 1; x < Width - 1; x++)
+                {
+                    if ((x % 2 == 0) && (y % 2 == 0))
+                    {
+                        ImgData[x, y, 0] = (ImgData[x + 1, y, 0] + ImgData[x - 1, y, 0]) / 2;
+                        ImgData[x, y, 2] = (ImgData[x, y + 1, 2] + ImgData[x, y - 1, 2]) / 2;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 0))
+                    {
+                        ImgData[x, y, 1] = (ImgData[x + 1, y, 1] + ImgData[x - 1, y, 1] + ImgData[x, y + 1, 1] + ImgData[x, y - 1, 1]) / 4;
+                        ImgData[x, y, 2] = (ImgData[x + 1, y + 1, 2] + ImgData[x - 1, y - 1, 2] + ImgData[x - 1, y + 1, 2] + ImgData[x + 1, y - 1, 2]) / 4;
+                    }
+                    else if ((x % 2 == 0) && (y % 2 == 1))
+                    {
+                        ImgData[x, y, 0] = (ImgData[x + 1, y, 0] + ImgData[x - 1, y, 0] + ImgData[x, y + 1, 0] + ImgData[x, y - 1, 0]) / 4;
+                        ImgData[x, y, 1] = (ImgData[x + 1, y + 1, 1] + ImgData[x - 1, y - 1, 1] + ImgData[x - 1, y + 1, 1] + ImgData[x + 1, y - 1, 1]) / 4;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 1))
+                    {
+                        ImgData[x, y, 0] = (ImgData[x, y + 1, 0] + ImgData[x, y - 1, 0]) / 2;
+                        ImgData[x, y, 2] = (ImgData[x + 1, y, 2] + ImgData[x - 1, y, 2]) / 2;
+                    }
+                }
+            }
+        }
+        private void btnEPBI_Click(object sender, EventArgs e) {
+            int[, ,] ImgData = GetImgData(CFABitmap);
+            EPBIProcess(ImgData);
+            Bitmap processedBitmap = CreateBitmap(ImgData);
+            picEPBI.Image = processedBitmap;
+
+            int[, ,] OriginalData = GetImgData(originalBitmap);
+            lblPSNR_EPBI.Text = Convert.ToString(PSNR(OriginalData, ImgData));
+        }
+        private void EPBIProcess(int[, ,] ImgData) {
+            int Width = ImgData.GetLength(0);
+            int Height = ImgData.GetLength(1);
+            for (int y = 1 ; y < Height - 1 ; y++) {
+                for (int x = 1 ; x < Width - 1 ; x++) {
+                    if ((x % 2 == 0) && (y % 2 == 0)) {
+                        ImgData[x, y, 0] = (ImgData[x + 1, y, 0] + ImgData[x - 1, y, 0]) / 2;
+                        ImgData[x, y, 2] = (ImgData[x, y + 1, 2] + ImgData[x, y - 1, 2]) / 2;
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 0)) {
+                        ImgData[x, y, 1] = Mid(ImgData[x + 1, y, 1], ImgData[x - 1, y, 1], ImgData[x, y + 1, 1], ImgData[x, y - 1, 1]);
+                        ImgData[x, y, 2] = Mid(ImgData[x + 1, y + 1, 2], ImgData[x - 1, y - 1, 2], ImgData[x - 1, y + 1, 2], ImgData[x + 1, y - 1, 2]);
+                    }
+                    else if ((x % 2 == 0) && (y % 2 == 1)) {
+                        ImgData[x, y, 0] = Mid(ImgData[x + 1, y, 0], ImgData[x - 1, y, 0], ImgData[x, y + 1, 0], ImgData[x, y - 1, 0]);
+                        ImgData[x, y, 1] = Mid(ImgData[x + 1, y + 1, 1], ImgData[x - 1, y - 1, 1], ImgData[x - 1, y + 1, 1], ImgData[x + 1, y - 1, 1]);
+                    }
+                    else if ((x % 2 == 1) && (y % 2 == 1)) {
+                        ImgData[x, y, 0] = (ImgData[x, y + 1, 0] + ImgData[x, y - 1, 0]) / 2;
+                        ImgData[x, y, 2] = (ImgData[x + 1, y, 2] + ImgData[x - 1, y, 2]) / 2;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
